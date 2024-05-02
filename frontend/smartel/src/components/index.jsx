7e81d8b8b208ae6449f6309645cae5e4355d1944
Aknur Kassym <kassym.aknur@gmail.com>
2024-05-02 17:21:03 +0400

    feat: add a custom CalendarView component

--- frontend/smartel/src/components/index.jsx
+++ frontend/smartel/src/components/index.jsx
@@ -3,3 +3,5 @@ export { default as NavBar } from './NavBar';
 export { default as ProtectedRoute } from './ProtectedRoute';
 export { default as RouteButton } from './RouteButton';
 export { default as Button } from './Button';
+export { default as CalendarView } from './CalendarView';
+export { default as Modal } from './Modal';
